2ef65f0097ac948b96e613de2ad11e1741670ab2
sushilpal3482-byte <sushilpal3482@gmail.com>
2026-08-07 21:09:02 +0530

    Update data.js

--- data.js
+++ data.js
@@ -44,7 +44,7 @@ const siteData = {
       items: [
         { name: "Jalebi (100g)", nameHi: "जलेबी", price: 25, desc: "Hot, crispy, and sweet.", image: "images/JALEBI.jpeg" },
         { name: "Vada Pav (2 pcs)", nameHi: "वड़ा पाव", price: 20, desc: "Classic street-style Vada Pav.", image: "images/VADA%20PAV.jpeg" },
-        { name: "Samosa (1 pc)", nameHi: "समोसा", price: 10, desc: "Crispy potato pastry.", image: "images/SAMOSA-PAV.jpeg" },
+        { name: "Samosa (1 pc)", nameHi: "समोसा", price: 10, desc: "Crispy potato pastry.", image: "images/SAMOSA.jpeg" },
         { name: "Samosa Pav", nameHi: "समोसा पाव", price: 15, desc: "Crispy samosa served inside a soft bread roll with chutneys.", image: "images/SAMOSA-PAV.jpeg" },
         { name: "Samosa Bucket (7 pcs)", nameHi: "समोसा बकेट", price: 60, desc: "Perfect for sharing.", image: "images/SAMOSA%20BUCKET.png" },
         { name: "Cutlet (1 pc)", nameHi: "कटलेट", price: 15, desc: "Crispy vegetable cutlet.", image: "images/BREAD%20CUTLET.jpeg" }
